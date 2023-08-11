7d28c930aede9e41a1496a394b049df247770494
Daniela Cruz <dccruzt@gmail.com>
2023-08-11 17:41:30 +0400

    chore: refactor logic

--- lib/src/utils/util.dart
+++ lib/src/utils/util.dart
@@ -29,19 +29,19 @@ class Utils {
     required String value,
   }) {
     if (value.isNotEmpty) {
-      final filteredCountries = countries
-          .where(
-            (Country country) =>
-                country.alpha3Code!
-                    .toLowerCase()
-                    .startsWith(value.toLowerCase()) ||
-                country.name!.toLowerCase().contains(value.toLowerCase()) ||
-                Utils.getCountryName(country, locale)!
-                    .toLowerCase()
-                    .contains(value.toLowerCase()) ||
-                country.dialCode!.contains(value.toLowerCase()),
-          )
-          .toList();
+      final filteredCountries = countries.where(
+        (Country country) {
+          return country.alpha3Code!
+                  .toLowerCase()
+                  .startsWith(value.toLowerCase()) ||
+              Utils.getCountryName(country, locale)!
+                  .toLowerCase()
+                  .contains(value.toLowerCase()) ||
+              country.dialCode!.contains(value.toLowerCase());
+        },
+      ).toList();
+
+      if (filteredCountries.isEmpty) return filteredCountries;
 
       final matchDialCode = filteredCountries[0].dialCode!.contains(value);
       final matchLocaleName =
@@ -53,25 +53,19 @@ class Utils {
 
       return filteredCountries
         ..sort((a, b) {
-          final compareLengthDialCode =
+          final compareDialCodeLength =
               a.dialCode!.length.compareTo(b.dialCode!.length);
-          final compareLengthName = Utils.getCountryName(a, locale)!
+          final compareLocalNameLength = Utils.getCountryName(a, locale)!
               .toLowerCase()
               .length
               .compareTo(Utils.getCountryName(b, locale)!.toLowerCase().length);
 
-          if (matchDialCode && compareLengthDialCode != 0) {
-            return compareLengthDialCode;
-          } else if (matchLocaleName && compareLengthName != 0) {
-            return compareLengthName;
+          if (matchDialCode && compareDialCodeLength != 0) {
+            return compareDialCodeLength;
+          } else if (matchLocaleName && compareLocalNameLength != 0) {
+            return compareLocalNameLength;
           }
-
-          return matchDialCode
-              ? a.dialCode!.compareTo(b.dialCode!)
-              : Utils.getCountryName(a, locale)!.toLowerCase().length.compareTo(
-                  Utils.getCountryName(b, locale)!
-                      .toLowerCase()
-                      .length); // Sort lexicographically
+          return 0;
         });
     }
 
