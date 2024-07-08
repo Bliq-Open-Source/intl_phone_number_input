c84a967f21dab0b652cab99e50c2e9f5f91b29f0
Eduardo Baiao <eduardo.baiao@bliq.ai>
2024-07-08 16:20:39 +0200

    Make the bottom sheet use the root navigator so that it can be dismissed with the back button.

--- lib/src/widgets/selector_button.dart
+++ lib/src/widgets/selector_button.dart
@@ -162,6 +162,7 @@ class SelectorButton extends StatelessWidget {
           borderRadius: BorderRadius.only(
               topLeft: Radius.circular(12), topRight: Radius.circular(12))),
       useSafeArea: selectorConfig.useBottomSheetSafeArea,
+      useRootNavigator: true,
       builder: (BuildContext context) {
         return Stack(children: [
           GestureDetector(
